085199904b3ec8b304bd59fb3935ee3d25ae2ab4
eraste-dev <keraste38@gmail.com>
2025-10-16 17:48:35 +0000

    MAJ

--- wp-content/plugins/tekprof-toolkit/includes/elementor/elementor-templates/about-two.php
+++ wp-content/plugins/tekprof-toolkit/includes/elementor/elementor-templates/about-two.php
@@ -5,7 +5,7 @@
             <div class="about-inner pb-100 rpb-70">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-10">
+                        <div class="col-lg-12">
                             <div class="section-title mb-50 rmb-0" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="50">
                                 <?php if ($settings['layout_two_sub_title']) : ?>
                                     <span class="sub-title mb-10"><?php echo rt_kses_basic($settings['layout_two_sub_title']); ?></span>
@@ -18,18 +18,8 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-7">
-                            <div class="about-images" data-aos="fade-left" data-aos-duration="1500" data-aos-offset="50">
-                                <div class="left">
-                                    <?php rt_elementor_rendered_image($settings, 'layout_two_image_one'); ?>
-                                </div>
-                                <div class="right">
-                                    <?php rt_elementor_rendered_image($settings, 'layout_two_image_two'); ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-5">
-                            <div class="about-content mb-30 rmt-20" data-aos="fade-right" data-aos-duration="1500" data-aos-offset="50">
+                        <div class="col-lg-6">
+                            <div class="about-content mb-30" data-aos="fade-right" data-aos-duration="1500" data-aos-offset="50">
                                 <?php if ($settings['layout_two_summary_text']) : ?>
                                     <p><?php echo rt_kses_basic($settings['layout_two_summary_text']); ?></p>
                                 <?php endif; ?>
@@ -41,6 +31,16 @@
                                 <?php if (!empty($settings['layout_two_button_label'])) : ?>
                                     <a href="<?php echo esc_url($settings['layout_two_button_url']['url']); ?>" class="theme-btn style-three mt-55 rmt-40" <?php echo ($settings['layout_two_button_url']['is_external']) ? 'target="_blank"' : ''; ?> <?php echo ($settings['layout_two_button_url']['nofollow']) ? 'rel="nofollow"' : ''; ?>><?php echo esc_html($settings['layout_two_button_label']); ?></a>
                                 <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="about-images" data-aos="fade-left" data-aos-duration="1500" data-aos-offset="50">
+                                <div class="left">
+                                    <?php rt_elementor_rendered_image($settings, 'layout_two_image_one'); ?>
+                                </div>
+                                <div class="right">
+                                    <?php rt_elementor_rendered_image($settings, 'layout_two_image_two'); ?>
+                                </div>
                             </div>
                         </div>
                     </div>
--- wp-content/plugins/tekprof-toolkit/includes/elementor/elementor-templates/service-details-two.php
+++ wp-content/plugins/tekprof-toolkit/includes/elementor/elementor-templates/service-details-two.php
@@ -1,49 +1,75 @@
 <?php if ('layout_two' == $settings['layout_type']) : ?>
     <!-- Modern IT Solutions Area start -->
-    <section class="modern-it-solutions-area rel z-1">
+    <section class="modern-it-solutions-area-v2 rel z-1">
         <div class="container-fluid">
-            <div class="row gap-10">
+            <div class="row align-items-stretch g-4">
                 <?php if (!empty($settings['layout_two_image'])) : ?>
-                    <div class="col-xl-4 col-lg-8">
-                        <div class="image" data-aos="fade-left" data-aos-duration="1500" data-aos-offset="50">
-                            <?php echo rt_elementor_rendered_image($settings, 'layout_two_image'); ?>
+                    <div class="col-xl-5 col-lg-12">
+                        <div class="solutions-image-card" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="50">
+                            <div class="image-wrapper">
+                                <?php echo rt_elementor_rendered_image($settings, 'layout_two_image'); ?>
+                                <div class="image-overlay"></div>
+                            </div>
                         </div>
                     </div>
                 <?php endif; ?>
-                <div class="col-xl-8">
-                    <div class="it-solutions-right-content bgc-black">
-                        <div class="row align-items-center">
+                <div class="col-xl-7 col-lg-12">
+                    <div class="solutions-content-card">
+                        <div class="row g-4">
                             <div class="col-lg-6">
-                                <div class="left-content rmb-50" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="50">
-                                    <div class="section-title mb-50 rmb-40">
-                                        <?php if (!empty($settings['layout_two_subtitle'])) : ?>
-                                            <span class="sub-title color-primary mb-10"><?php echo rt_kses_basic($settings['layout_two_subtitle']); ?></span>
-                                        <?php endif; ?>
-                                        <?php if (!empty($settings['layout_two_title'])) : ?>
-                                            <<?php echo esc_attr($settings['layout_two_title_tag']); ?>><?php echo rt_kses_basic($settings['layout_two_title']); ?></<?php echo esc_attr($settings['layout_two_title_tag']); ?>>
-                                        <?php endif; ?>
-                                    </div>
-                                    <?php if (!empty($settings['layout_two_description'])) : ?>
-                                        <p><?php echo rt_kses_basic($settings['layout_two_description']); ?></p>
+                                <div class="content-header-section" data-aos="fade-up" data-aos-duration="800" data-aos-offset="50">
+                                    <?php if (!empty($settings['layout_two_subtitle'])) : ?>
+                                        <div class="subtitle-badge">
+                                            <span class="badge-dot"></span>
+                                            <span class="badge-text"><?php echo rt_kses_basic($settings['layout_two_subtitle']); ?></span>
+                                        </div>
                                     <?php endif; ?>
+
+                                    <?php if (!empty($settings['layout_two_title'])) : ?>
+                                        <<?php echo esc_attr($settings['layout_two_title_tag']); ?> class="content-main-title"><?php echo rt_kses_basic($settings['layout_two_title']); ?></<?php echo esc_attr($settings['layout_two_title_tag']); ?>>
+                                    <?php endif; ?>
+
+                                    <?php if (!empty($settings['layout_two_description'])) : ?>
+                                        <p class="content-description"><?php echo rt_kses_basic($settings['layout_two_description']); ?></p>
+                                    <?php endif; ?>
+
                                     <?php if (!empty($settings['layout_two_button_text']) && !empty($settings['layout_two_button_link']['url'])) : ?>
-                                        <a href="<?php echo esc_url($settings['layout_two_button_link']['url']); ?>" <?php if (!empty($settings['layout_two_button_link']['is_external'])) : ?> target="_blank" <?php endif; ?> class="theme-btn mt-35 rmt-20"><?php echo rt_kses_basic($settings['layout_two_button_text']); ?></a>
+                                        <div class="content-action">
+                                            <a href="<?php echo esc_url($settings['layout_two_button_link']['url']); ?>"
+                                               <?php if (!empty($settings['layout_two_button_link']['is_external'])) : ?> target="_blank" <?php endif; ?>
+                                               class="modern-action-btn">
+                                                <span class="btn-text"><?php echo rt_kses_basic($settings['layout_two_button_text']); ?></span>
+                                                <span class="btn-icon">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M4.16669 10H15.8334" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        <path d="M10 4.16666L15.8333 10L10 15.8333" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </span>
+                                            </a>
+                                        </div>
                                     <?php endif; ?>
                                 </div>
                             </div>
+
                             <div class="col-lg-6">
-                                <div class="it-solutions-wrap" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="50">
+                                <div class="features-grid" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="50">
                                     <?php if (!empty($settings['layout_two_features'])) : ?>
-                                        <?php foreach ($settings['layout_two_features'] as $feature) : ?>
-                                            <div class="it-solution-item">
-                                                <?php if (!empty($feature['feature_title'])) : ?>
-                                                    <h5><?php echo rt_kses_basic($feature['feature_title']); ?></h5>
-                                                <?php endif; ?>
-                                                <?php if (!empty($feature['feature_description'])) : ?>
-                                                    <p><?php echo rt_kses_basic($feature['feature_description']); ?></p>
-                                                <?php endif; ?>
+                                        <?php $index = 1; foreach ($settings['layout_two_features'] as $feature) : ?>
+                                            <div class="feature-card-modern" data-aos="fade-left" data-aos-duration="<?php echo 600 + ($index * 100); ?>" data-aos-delay="<?php echo $index * 100; ?>">
+                                                <div class="feature-number">
+                                                    <span><?php echo sprintf('%02d', $index); ?></span>
+                                                </div>
+                                                <div class="feature-content">
+                                                    <?php if (!empty($feature['feature_title'])) : ?>
+                                                        <h5 class="feature-title"><?php echo rt_kses_basic($feature['feature_title']); ?></h5>
+                                                    <?php endif; ?>
+                                                    <?php if (!empty($feature['feature_description'])) : ?>
+                                                        <p class="feature-text"><?php echo rt_kses_basic($feature['feature_description']); ?></p>
+                                                    <?php endif; ?>
+                                                </div>
+                                                <div class="feature-accent"></div>
                                             </div>
-                                        <?php endforeach; ?>
+                                        <?php $index++; endforeach; ?>
                                     <?php endif; ?>
                                 </div>
                             </div>
@@ -51,6 +77,12 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- Background Decorations -->
+        <div class="solutions-bg-shapes">
+            <div class="shape shape-1"></div>
+            <div class="shape shape-2"></div>
         </div>
     </section>
     <!-- Modern IT Solutions Area end -->
